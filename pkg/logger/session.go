59f4d3021aac7e108e806999cba76b33a48e4394
michaelladouceur1 <michaelladouceurdev@gmail.com>
2024-10-21 22:34:06 -0500

    minor change to LogSessionInfo

--- pkg/logger/session.go
+++ pkg/logger/session.go
@@ -17,14 +17,8 @@ func LogSessionEnd(session models.SessionFile, task models.Task) {
 }
 
 func LogSessionInfo(session models.SessionFile, task models.Task) {
+	startTime := session.StartTime.Round(time.Second).Format("15:04:05")
 	timeSince := time.Since(session.StartTime).Round(time.Second)
-
-	var startTime string
-	if timeSince.Hours() > 12 {
-		startTime = session.StartTime.Round(time.Second).Format("2006-01-02 15:04:05")
-	} else {
-		startTime = session.StartTime.Round(time.Second).Format("15:04:05")
-	}
 
 	fmt.Printf("Current session for task: %s \n", task.Title)
 	fmt.Printf("Start time: %s \n", startTime)
